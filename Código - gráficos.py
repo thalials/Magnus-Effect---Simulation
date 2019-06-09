bb8f0ac65e18ce6e82e3a95c13cd2c0d77eda4ab
Thalia Loiola <thalials@al.insper.edu.br>
2019-06-08 23:32:05 -0300

    algumas correcoes

--- Código - gráficos.py
+++ Código - gráficos.py
@@ -3,21 +3,19 @@
 Autoria:
  Cicero Tiago Carneiro Valentim 
  Thalia Loiola Silva
+
 """
 from math import sqrt, pi
 from scipy.integrate import odeint
 import numpy as np
 import matplotlib.pyplot as plt
-
-#r = 0.121  	
+ 	
 r = 0.1225		# raio da bola (m)
 A = pi*(r**2)          	# Área transversal (m²)
 V = 4*pi*(r**3)/3   	# Volume da esfera (m³)
 ro = 1.17            	# densidade volumétrica (kg/m³)
-#Cd = 0.42           # Coeficiente de arrasto
 w = 12          # velocidade angular (rad/s)
 g = 10               # Aceleração gravitacional
-#m = 0.3 
 m = 0.62             # massa da esfera (kg)
 n = 1.8*1e-5   #viscosidade dinamica (Pa.s)
 I = (2/3)*m*(r**2)
@@ -31,12 +29,10 @@ def eq_dif( lista_equacoes, tempo):
 
     if y <= r:
         y = 0
-#        w = 0
         dxdt = 0
         dydt = 0
         dvxdt = 0
         dvydt = 0
-#        dwdt = 0 
         
     else:
         v = sqrt((vx**2) + (vy**2))
@@ -76,14 +72,12 @@ lista_tempo = np.arange(0, 20, dt)
 condicoes_iniciais = [0, 126.5, 0, 0]
 
 solucao = odeint(eq_dif,condicoes_iniciais,lista_tempo)
+
 # Solucoes 
 x = solucao[:,0]
 y = solucao[:,1]
 vx = solucao[:,2]
 vy = solucao[:,3]
-#w = solucao[:,4]
-#print(w[-1])
-print(y[-1])
 
 #Graficos:
 # 1) Gráfico da trajetória 
@@ -91,7 +85,6 @@ print(y[-1])
 #plt.title("w versus tempo")
 #plt.xlabel('tempo')
 #plt.ylabel('w')
-#
 #plt.grid()
 #plt.show()
 
