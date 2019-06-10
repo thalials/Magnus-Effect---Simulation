4fe34bcb460daecfd0be486a9387096013b56ffd
cicerotcv <49568618+cicerotcv@users.noreply.github.com>
2019-06-10 19:46:01 -0300

    gráfico 3D

--- Código - gráficos.py
+++ Código - gráficos.py
@@ -26,94 +26,120 @@ I = (2/3)*m*(r**2)
 def eq_dif(lista_equacoes, tempo):
     x = lista_equacoes[0]
     y = lista_equacoes[1]
+    z = lista_equacoes[2]
     
-    vx = lista_equacoes[2]
-    vy = lista_equacoes[3]
+    vx = lista_equacoes[3]
+    vy = lista_equacoes[4]
+    vz = lista_equacoes[5]
 
-    v = sqrt((vx**2) + (vy**2))
+    v = sqrt((vx**2) + (vy**2) + (vz**2))
     
     if y < 0:
         dxdt = 0
         dydt = 0
+        dzdt = 0
         dvxdt = 0
         dvydt = 0
+        dvzdt = 0
        
     else:
         
         dxdt = vx
         dydt = vy
+        dzdt = vz
        
         if v > 0 :
-            Cl = 0.126 #o coeficiente n depende de w pq esse valor já tem o w incluso (valor experimental)
-            Re = 2*ro*r*v/n
-            Cd = 0.25 #24/Re + (2.6*(Re/5))/(1 + ((Re/5)**1.52)) + 0.411*((Re/263000)**(-7.94))/(1+((Re/263000)**(-8))) + (Re**0.8)/461000
-            cos_alfa = vx/v
-            sen_alfa = vy/v
+            Cl = 0.12 # o coeficiente n depende de w pq esse valor já tem o w incluso (valor experimental)
+            Cd = 0.25 
             
-            Fd = ro*(v**2)*A*Cd/2
-            Fm = ro*(v**2)*A*Cl/2
+            D = -ro*(v**2)*A*Cd/2
+            Dx = D*vx/v
+            Dy = D*vy/v
+            Dz = D*vz/v
             
-            Rx = -Fd*cos_alfa - Fm*sen_alfa
+            M = -ro*(v**2)*A*Cl/2
+            Mx = M*(vz-vy)/v
+            My = M*vx/v
+            Mz = -M*vx/v
+            
+            Rx = Dx + Mx
+            Ry = Dy + My - m*g
+            Rz = Dz + Mz
+            
             dvxdt = Rx/m
-            
-            Ry = -Fd*sen_alfa + Fm*cos_alfa - m*g
             dvydt = Ry/m
+            dvzdt = Rz/m
+
 
         elif v == 0:
-            Fd = 0
-            Fm = 0
+            D = 0
+            M = 0
             Ry = -m*g
             dvxdt = 0
             dvydt = Ry/m
+            dvzdt = 0
 
         
-    return dxdt, dydt, dvxdt, dvydt
+    return dxdt, dydt, dzdt, dvxdt, dvydt, dvzdt
 
 def eq_dif2(lista_equacoes, tempo):
     x = lista_equacoes[0]
     y = lista_equacoes[1]
+    z = lista_equacoes[2]
     
-    vx = lista_equacoes[2]
-    vy = lista_equacoes[3]
+    vx = lista_equacoes[3]
+    vy = lista_equacoes[4]
+    vz = lista_equacoes[5]
 
-    v = sqrt((vx**2) + (vy**2))
+    v = sqrt((vx**2) + (vy**2) + (vz**2))
     
     if y < 0:
         dxdt = 0
         dydt = 0
+        dzdt = 0
         dvxdt = 0
         dvydt = 0
+        dvzdt = 0
        
     else:
         
         dxdt = vx
         dydt = vy
+        dzdt = vz
        
         if v > 0 :
             Cl = 0 # o coeficiente n depende de w pq esse valor já tem o w incluso (valor experimental)
-#            Re = 2*ro*r*v/n
-            Cd = 0.25 # 24/Re + (2.6*(Re/5))/(1 + ((Re/5)**1.52)) + 0.411*((Re/263000)**(-7.94))/(1+((Re/263000)**(-8))) + (Re**0.8)/461000
-            cos_alfa = vx/v
-            sen_alfa = vy/v
+            Cd = 0.25 
             
-            Fd = ro*(v**2)*A*Cd/2
-            Fm = ro*(v**2)*A*Cl/2
+            D = -ro*(v**2)*A*Cd/2
+            Dx = D*vx/v
+            Dy = D*vy/v
+            Dz = 0
             
-            Rx = -Fd*cos_alfa - Fm*sen_alfa
+            M = -ro*(v**2)*A*Cl/2
+            Mx = M*(vz-vy)/v
+            My = M*vx/v
+            Mz = 0
+            
+            Rx = Dx + Mx
+            Ry = Dy + My - m*g
+            Rz = Dz + Mz
+            
             dvxdt = Rx/m
-            
-            Ry = -Fd*sen_alfa + Fm*cos_alfa - m*g
             dvydt = Ry/m
+            dvzdt = 0
+
 
         elif v == 0:
-            Fd = 0
-            Fm = 0
+            D = 0
+            M = 0
             Ry = -m*g
             dvxdt = 0
             dvydt = Ry/m
+            dvzdt = 0
 
         
-    return dxdt, dydt, dvxdt, dvydt
+    return dxdt, dydt, dzdt, dvxdt, dvydt, dvzdt
 
 #lista de tempo analisado
 dt = 1e-4
@@ -122,21 +148,19 @@ lista_tempo = np.arange(0, 15, dt)
 # Condicoes iniciais: x, y, vx, vy respectivamente
 v = 76 # velocidade de lançamento (m/s)
 theta = 17 # angulo de lançamento (graus)
-condicoes_iniciais = [0, 0, v*cos(theta*pi/180), v*sin(theta*pi/180)]
+condicoes_iniciais = [0, 0, 0, v*cos(theta*pi/180), v*sin(theta*pi/180), 0]
 
-solucao = odeint(eq_dif, condicoes_iniciais, lista_tempo) # com efeito Magnus e Arrasto
-solucao2 = odeint(eq_dif2, condicoes_iniciais, lista_tempo) # somente c om Arrasto
 
 # Solucoes
-for i in range(8):
-    theta = 3 + 11.3*i
-    condicoes_iniciais = [0, 0, v*cos(theta*pi/180), v*sin(theta*pi/180)]
-    solucao = odeint(eq_dif, condicoes_iniciais, lista_tempo) # com efeito Magnus e Arrasto
-    x = solucao[:,0]
-    y = solucao[:,1]
-    vx = solucao[:,2]
-    vy = solucao[:,3]
-    plt.plot(x, y, label = '{:.0f}'.format(theta)) 
+solucao2 = odeint(eq_dif2, condicoes_iniciais, lista_tempo) # somente c om Arrasto
+condicoes_iniciais = [0, 0, 0, v*cos(theta*pi/180), v*sin(theta*pi/180),0]
+solucao = odeint(eq_dif, condicoes_iniciais, lista_tempo) # com efeito Magnus e Arrasto
+x = solucao[:,0]
+y = solucao[:,1]
+z = solucao[:,2]
+vx = solucao[:,2]
+vy = solucao[:,3]
+vz = solucao[:,4]
 
 x1 = solucao2[:,0]
 y1 = solucao2[:,1]
@@ -144,6 +168,8 @@ vx1 = solucao2[:,2]
 vy1 = solucao2[:,3]
 
 ##Graficos:
+
+plt.plot(x, y, label = '{:.0f}'.format(theta)) 
 plt.plot(x1, y1, label = 'Sem Efeito Magnus') #nesse caso devemos usar as equações da fisica normal 
 plt.title("Trajetória")
 plt.xlabel("x (m)")
@@ -153,49 +179,13 @@ plt.grid(True)
 plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 plt.show()
 
-#
-#plt.plot(vx, vy, '--')
-#plt.xlabel("vx")
-#plt.ylabel("vy")
-#plt.axis("equal")
-#plt.grid()
-#plt.show()
-##
-#plt.plot(lista_tempo,solucao[:,1])
-#plt.title("y vs tempo")
-#plt.xlabel("tempo")
-#plt.ylabel("y")
-#plt.grid()
-#plt.show()
-#
-#plt.plot(lista_tempo,solucao[:,0])
-#plt.title("x vs tempo")
-#plt.xlabel("tempo")
-#plt.ylabel("x")
-#plt.grid()
-#plt.show()
-#
-#plt.plot(lista_tempo,solucao[:,2])
-#plt.title("vx vs tempo")
-#plt.xlabel("tempo")
-#plt.ylabel("Velocidade horizontal")
-#plt.grid()
-#plt.show()
-##
-#plt.plot(lista_tempo,solucao[:,3])
-#plt.title("vy vs tempo")
-#plt.xlabel("tempo")
-#plt.ylabel("Velocidade vertical")
-#plt.grid()
-#plt.show()
 
-#fig=p.figure()
-#ax = p3.Axes3D(fig)
-#ax.plot_wireframe(x2,y2,z2)
-#ax.set_xlabel(r'')
-#ax.set_ylabel(r'')
-#ax.set_zlabel(r'')
-#
-#x2 = 10*np.outer(np.ones(np.size(solucao[:,0])))
-#y2 = 10*np.outer(np.ones(np.size(solucao[:,1])))
-#z2 = 10*np.outer(np.ones(np.size(solucao[:,0])))
+fig=p.figure()
+ax = p3.Axes3D(fig)
+ax.plot(solucao[:,0], solucao[:,2], solucao[:,1], label = "Com Efeito Magnus")
+ax.plot(solucao2[:,0], solucao2[:,2], solucao2[:,1],'r', label = "Sem Efeito Magnus")
+ax.set_xlabel("x (m)")
+ax.set_ylabel("z (m)")
+ax.set_zlabel("y (m)")
+ax.set_title("Trajetória da bola")
+plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
